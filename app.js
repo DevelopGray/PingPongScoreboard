b25159233baa18f70627af2267edeaa5b385aadb
DevelopGray <developgray@gmail.com>
2024-07-26 13:48:27 -0500

    Updated files

--- app.js
+++ app.js
@@ -1,36 +1,9 @@
-function setCookie(pOneScore, pTwoScore, maximumScore, exdays) {
+function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
   let expires = "expires=" + d.toUTCString();
-  document.cookie =
-    "playerOneScore" +
-    "=" +
-    pOneScore +
-    ";" +
-    "playerTwoScore" +
-    "=" +
-    pTwoScore +
-    ";" +
-    "maxScore" +
-    "=" +
-    maximumScore +
-    ";" +
-    expires +
-    ";path=/";
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-
-const playerOneBtn = document.querySelector("#playerOneBtn");
-const playerTwoBtn = document.querySelector("#playerTwoBtn");
-const resetBtn = document.querySelector("#resetBtn");
-
-const playerOneScoreText = document.querySelector("#player1");
-const playerTwoScoreText = document.querySelector("#player2");
-
-let playerOneScore = parseInt(playerOneScoreText.innerText);
-let playerTwoScore = parseInt(playerTwoScoreText.innerText);
-
-playTill = document.querySelector("#playToNum");
-let maxScore = parseInt(playTill.value);
 
 function getCookie(cname) {
   let name = cname + "=";
@@ -48,23 +21,71 @@ function getCookie(cname) {
   return "";
 }
 
-//
+function deleteAllCookies() {
+  for (let x of checkList) {
+    if (getCookie(x) === "") {
+      return;
+    } else {
+      var c = document.cookie.split("; ");
+      for (i in c)
+        document.cookie =
+          /^[^=]+/.exec(c[i])[0] + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+  }
+}
 
-//
+const playerOneScoreText = document.querySelector("#player1");
+const playerTwoScoreText = document.querySelector("#player2");
+const playTill = document.querySelector("#playToNum");
+const playerOneBtn = document.querySelector("#playerOneBtn");
+const playerTwoBtn = document.querySelector("#playerTwoBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+const checkList = ["playerOneScore", "playerTwoScore", "maxScore"];
+let scores = {
+  playerOneScore: parseInt(playerOneScoreText.innerText),
+  playerTwoScore: parseInt(playerTwoScoreText.innerText),
+  maxScore: parseInt(playTill.value),
+};
+
+for (let x of checkList) {
+  if (getCookie(x) === "") {
+    if (x === "maxScore") {
+      setCookie(x, 5, 365);
+    } else {
+      setCookie(x, 0, 365);
+    }
+  } else {
+    scores[x] = parseInt(getCookie(x));
+    if (scores.playerOneScore !== 0 || scores.playerTwoScore !== 0) {
+      playTill.disabled = true;
+    }
+    if (
+      scores.playerOneScore >= scores.maxScore ||
+      scores.playerTwoScore >= scores.maxScore
+    ) {
+      gameOver();
+    }
+  }
+}
+
+playerOneScoreText.innerText = scores.playerOneScore;
+playerTwoScoreText.innerText = scores.playerTwoScore;
+playTill.value = scores.maxScore;
 
 resetBtn.addEventListener("click", () => {
   resetGame();
 });
 
 playerOneBtn.addEventListener("click", () => {
-  playerOneScore += 1;
-  //
-  setCookie(playerOneScore, playerTwoScore, maxScore, 365);
-  console.log(document.cookie);
-  //
+  playTill.disabled = true;
+  scores.playerOneScore = parseInt(scores.playerOneScore) + 1;
+  // ==================================================================
+  setCookie("playerOneScore", scores.playerOneScore, 365);
+  // ==================================================================
   playerOneScoreText.classList.add("flip");
   setTimeout(removeFlipPlayerOne, 250);
-  if (playerOneScore == maxScore) {
+  if (scores.playerOneScore === scores.maxScore) {
     gameOver();
     playerOneScoreText.classList.add("winner");
     playerTwoScoreText.classList.add("loser");
@@ -72,33 +93,34 @@ playerOneBtn.addEventListener("click", () => {
 });
 
 playerTwoBtn.addEventListener("click", () => {
-  playerTwoScore += 1;
-  //
-  setCookie(playerOneScore, playerTwoScore, maxScore, 365);
-  console.log(document.cookie);
-  //
+  playTill.disabled = true;
+  scores.playerTwoScore = parseInt(scores.playerTwoScore) + 1;
+  // ==================================================================
+  setCookie("playerTwoScore", scores.playerTwoScore, 365);
+  // ==================================================================
   playerTwoScoreText.classList.add("flip");
   setTimeout(removeFlipPlayerTwo, 250);
-  if (playerTwoScore == maxScore) {
+  if (scores.playerTwoScore === scores.maxScore) {
     gameOver();
     playerTwoScoreText.classList.add("winner");
     playerOneScoreText.classList.add("loser");
   }
 });
 
+playTill.addEventListener("change", () => {
+  scores.maxScore = parseInt(playTill.value);
+  setCookie("maxScore", scores.maxScore, 365);
+});
+
 function removeFlipPlayerOne() {
-  playerOneScoreText.innerText = playerOneScore;
+  playerOneScoreText.innerText = scores.playerOneScore;
   playerOneScoreText.classList.remove("flip");
 }
 
 function removeFlipPlayerTwo() {
-  playerTwoScoreText.innerText = playerTwoScore;
+  playerTwoScoreText.innerText = scores.playerTwoScore;
   playerTwoScoreText.classList.remove("flip");
 }
-
-playTill.addEventListener("change", () => {
-  maxScore = parseInt(playTill.value);
-});
 
 function gameOver() {
   playerOneBtn.disabled = true;
@@ -120,8 +142,10 @@ function resetNums() {
 }
 
 function resetGame() {
-  playerOneScore = 0;
-  playerTwoScore = 0;
+  scores.playerOneScore = 0;
+  scores.playerTwoScore = 0;
+  scores.maxScore = 5;
+  playTill.value = scores.maxScore;
   playerOneScoreText.classList.add("flip");
   playerTwoScoreText.classList.add("flip");
   setTimeout(removeFlipPlayerOne, 250);
@@ -129,4 +153,6 @@ function resetGame() {
   setTimeout(resetNums, 250);
   playerOneBtn.disabled = false;
   playerTwoBtn.disabled = false;
+  playTill.disabled = false;
+  deleteAllCookies();
 }
